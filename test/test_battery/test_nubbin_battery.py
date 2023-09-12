7174b29a4656f4786ceed96a2b591d95515a699b
Jantoom <75770311+Jantoom@users.noreply.github.com>
2023-09-12 18:50:30 +1000

    Refactored unit tests, separated by car part.

---
+++ test/test_battery/test_nubbin_battery.py
@@ -0,0 +1,16 @@
+import unittest
+from datetime import datetime
+from src.battery.nubbin_battery import NubbinBattery
+
+class TestNubbinBattery(unittest.TestCase):
+    def test_needs_service_true(self):
+        current_date = datetime.today()
+        last_service_date = current_date.replace(year=current_date.year - 5)
+        battery = NubbinBattery(current_date, last_service_date)
+        self.assertTrue(battery.needs_service())
+
+    def test_needs_service_false(self):
+        current_date = datetime.today()
+        last_service_date = current_date.replace(year=current_date.year - 3)
+        battery = NubbinBattery(current_date, last_service_date)
+        self.assertFalse(battery.needs_service())
